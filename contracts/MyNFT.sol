4242641628d7fff96c8e10c061677ae47441e436
Fernando Maximo Ferreira <fernandomaximof@Fernandos-MBP.hub>
2022-02-15 23:13:51 +1300

    deployed a simple NFT

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
  
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
  
 contract MyNFT is ERC721 {
    constructor() ERC721("MyNFT", "MNFT") {
