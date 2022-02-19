81fe496ce009749e293b43ec5e6d6c137bca8198
Fernando Maximo Ferreira <fernandomaximof@Fernandos-MBP.hub>
2022-02-19 15:30:09 +1300

    added try catch scripts

--- scripts/generate-bloodz.js
+++ scripts/generate-bloodz.js
@@ -2,10 +2,16 @@ const BloodzNFT = artifacts.require('BloodzNFT')
 
 module.exports = async callback => {
     const bdz = await BloodzNFT.deployed()
-    console.log('Creating requests on contract:', bdz.address)
-    const tx = await bdz.requestNewRandomCharacter("THE BLOOD TYPE")
-    const tx2 = await bdz.requestNewRandomCharacter("O NEGATIVE BLOOD")
-    // const tx3 = await bdz.requestNewRandomCharacter("The Chainlink Wizard")
-    // const tx4 = await bdz.requestNewRandomCharacter("The Chainlink Orc")
-    callback(tx.tx)
+    console.log('CREATING REQUESTS ON CONTRACT:', bdz.address)
+    try {
+        const tx = await bdz.requestNewRandomCharacter("THE BLOOD TYPE")
+        const tx2 = await bdz.requestNewRandomCharacter("O NEGATIVE BLOOD")
+        // const tx3 = await bdz.requestNewRandomCharacter("The Chainlink Wizard")
+        // const tx4 = await bdz.requestNewRandomCharacter("The Chainlink Orc")
+        callback(tx.tx)
+    }
+    catch(e) {
+        // console.log(e);
+        return callback(new Error(e), null);
+    }
 }
--- scripts/get-bloodz.js
+++ scripts/get-bloodz.js
@@ -2,7 +2,7 @@ const BloodzNFT = artifacts.require('BloodzNFT')
 
 module.exports = async callback => {
     const bdz = await BloodzNFT.deployed()
-    console.log('Let\'s get the overview of your character')
+    console.log('LET\'S GET THE OVERVIEW OF YOUR CHARACTER')
     const overview = await bdz.characters(0)
     console.log(overview)
     callback(overview.tx)
